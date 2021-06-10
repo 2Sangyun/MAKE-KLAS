0c77724e45e1565b2caf46cd65d5cd51b84ffde6
babful <alstn6787@gmail.com>
2021-06-11 01:10:12 +0900

    minsu 0610_2

---
+++ minsu/0610_2/hw_show_not_submit.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1
+{
+    public partial class hw_show_not_submit : System.Web.UI.Page
+    {
+        private string ID;
+        private MySqlConnection conn =
+           new MySqlConnection("Server=localhost;Database=KLAS;Uid=root;Pwd=alstn2512!;");
+        private MySqlCommand cmd = new MySqlCommand();
+
+        private string course_id, hw_id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ID = Request["current_id"];
+            course_id = Request["course_id"];
+            hw_id = Request["hw_id"];
+
+            cmd.Connection = conn;
+
+            
+            if (!Page.IsPostBack)
+            {
+                Display();
+            }
+        }
+        private void Display()
+        {
+            conn.Open();
+
+            cmd.CommandText = "select title from klas.course where course_id='" + course_id + "'";
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            this.lblCourse.Text = reader["title"].ToString();
+            reader.Close();
+
+            cmd.CommandText = "select title, date_format(init_date, '%Y-%m-%d %H:%i:%s') as init_date, date_format(deadline, '%Y-%m-%d %H:%i:%s') as deadline, submit_form, file, content " +
+                            "from klas.homework " +
+                            "where course_id='" + course_id + "' and hw_id='" + hw_id + "'";
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            this.lblTitle.Text = reader["title"].ToString();
+            this.lblDate.Text = reader["init_date"].ToString() + " ~ " + reader["deadline"].ToString();
+            this.contentBox.Text = reader["content"].ToString();
+            this.downBtn.Text = reader["file"].ToString();
+            this.lblForm.Text = reader["submit_form"].ToString();
+            reader.Close();
+
+            cmd.CommandText = "SELECT state " +
+                            "FROM klas.submit_hw " +
+                            "where student_id='" + ID + "' " +
+                            "and course_id='" + course_id + "' " +
+                            "and hw_id='" + hw_id + "'";
+            reader = cmd.ExecuteReader();
+            reader.Read();
+            this.lblState.Text = reader["state"].ToString();
+
+            conn.Close();
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            /* 제출버튼 누르면 submit_hw테이블에 insert 돼야함 */
+            conn.Open();
+            cmd.CommandText = "UPDATE `klas`.`submit_hw` " +
+                            "SET `title` = '" + txtTitle.Text + "', " +
+                                "`state` = '제출', " +
+                                "`content` = '" + txtContent.Text + "' " +
+                            "WHERE (`course_id` = '" + course_id + "') " +
+                            "and (`hw_id` = '" + hw_id + "') " +
+                            "and (`student_id` = '" + ID + "');";
+            cmd.ExecuteNonQuery();
+
+            /*파일 업로드 기능*/
+            if (FileUpload1.HasFile)
+            {
+                cmd.CommandText = "UPDATE `klas`.`submit_hw` " +
+                                "SET `file` = '" + FileUpload1.FileName + "' " +
+                                "WHERE (`course_id` = '" + course_id + "') " +
+                                "and (`hw_id` = '" + hw_id + "') " +
+                                "and (`student_id` = '" + ID + "');";
+                cmd.ExecuteNonQuery();
+                FileUpload1.SaveAs("C:\\uploads\\" + FileUpload1.FileName);
+            }
+
+            conn.Close();
+
+            Response.Redirect("hw_list.aspx?current_id=" + ID);
+        }
+       
+        protected void btnList_Click(object sender, EventArgs e)
+        {
+            //리스트 페이지로 이동 
+            Response.Redirect("./notice_list.aspx?current_id=" + ID);
+
+        }
+        protected void mainBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("WebMain.aspx?current_id=" + ID));
+        }
+
+        protected void logoutBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("WebForm1.aspx?"));
+        }
+
+        protected void downBtn_Click(object sender, EventArgs e)
+        {
+            string strFileName = downBtn.Text;
+            if (strFileName == "")
+                return;
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.AddHeader("Content-Disposition",
+              "attachment;filename=" +
+              Server.UrlPathEncode(strFileName));
+
+            //Response.WriteFile("C:\\uploads\\test.txt");
+            Response.WriteFile("C:\\uploads\\" + strFileName);
+            Response.End();
+
+        }
+    }
+}
---
+++ minsu/0610_2/hw_write.aspx.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace WebApplication1
+{
+    public partial class hw_write : System.Web.UI.Page
+    {
+        /* 이 줄은 개인 환경에 맞춰서 작성 */
+        private MySqlConnection con = new MySqlConnection("datasource = localhost; port=3306; username = root; password=alstn2512!");
+        private MySqlCommand cmd = new MySqlCommand();
+
+        private string ID;
+        private string subject;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ID = Request["current_id"];
+
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT title " +
+                            "FROM klas.course " +
+                            "where course_id in (select course_id " +
+                                              "from klas.teaches " +
+                                              "where ID = '" + ID + "')";
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            int i = 0;
+            while (reader.Read())
+            {
+                DropDownList1.Items[i].Text = reader["title"].ToString();
+                i++;
+            }
+
+            while (i < 8)
+            {
+                DropDownList1.Items[i].Enabled = false;
+                i++;
+            }
+
+            subject = DropDownList1.Items[0].ToString();
+
+            con.Close();
+            if (!Page.IsPostBack)
+            {
+                DisplayYear();
+                DisplayMonth();
+                DisplayDay(31);
+            }
+        }
+
+        protected void mainBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("WebMain.aspx?current_id=" + ID));
+        }
+
+        protected void logoutBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(string.Format("WebForm1.aspx?"));
+        }
+
+        protected void btnWrite_Click(object sender, EventArgs e)
+        {
+
+            /*쿼리 수정 해야함!*/
+            /*this.start_year.SelectedValue 를 이용해서 DB에서 Datetime 타입에 맞춰야할듯?*/
+            
+            string course_id;
+            int hw_id = 0;
+
+            subject = DropDownList1.SelectedItem.ToString();
+
+            con.Open();
+            cmd.CommandText = "SELECT course_id FROM klas.course where title='" + subject + "'";
+            MySqlDataReader rd = cmd.ExecuteReader();
+            rd.Read();
+            course_id = rd["course_id"].ToString();
+            rd.Close();
+
+            cmd.CommandText = "SELECT hw_id " +
+                            "FROM klas.homework " +
+                            "where course_id='" + course_id + "' " +
+                            "order by hw_id desc";
+            rd = cmd.ExecuteReader();
+            rd.Read();
+            if (rd.HasRows)
+            {
+                hw_id = Int32.Parse(rd["hw_id"].ToString());
+                hw_id++;
+            }
+            rd.Close();
+
+            string init_date = start_year.SelectedValue + "-" + start_month.SelectedValue + "-" + start_day.SelectedValue + " 00:00:00";
+            string deadline = finish_year.SelectedValue + "-" + finish_month.SelectedValue + "-" + finish_day.SelectedValue + " 23:59:59";
+
+            cmd.CommandText = "INSERT INTO `klas`.`homework` (`course_id`, " +
+                                                       "`hw_id`, " +
+                                                       "`title`, " +
+                                                       "`init_date`, " +
+                                                       "`deadline`, " +
+                                                       "`content`, " +
+                                                       "`submit_form`) " +
+                            "VALUES ('" + course_id + "', " +
+                                   "'" + hw_id + "', " +
+                                   "'" + txtTitle.Text + "', " +
+                                   "'" + init_date + "', " +
+                                   "'" + deadline + "', " +
+                                   "'" + txtContent.Text + "', " +
+                                   "'" + txtForm.Text + "');";
+            cmd.ExecuteNonQuery();
+
+            if (FileUpload1.HasFile)
+            {
+                cmd.CommandText = "UPDATE `klas`.`homework` " +
+                                "SET `file` = '" + FileUpload1.FileName + "' " +
+                                "WHERE (`course_id` = '" + course_id + "') " +
+                                "and (`hw_id` = '" + hw_id + "');";
+                cmd.ExecuteNonQuery();
+                FileUpload1.SaveAs("C:\\uploads\\" + FileUpload1.FileName);
+
+            }
+
+            List<string> students = new List<string>();
+            cmd.CommandText = "SELECT ID FROM klas.takes where course_id='" + course_id + "'";
+            rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                students.Add(rd["ID"].ToString());
+            }
+            rd.Close();
+
+            for(int i = 0; i < students.Count; ++i)
+            {
+                cmd.CommandText = "INSERT INTO `klas`.`submit_hw` (`student_id`, " +
+                                                            "`course_id`, " +
+                                                            "`hw_id`, " +
+                                                            "`state`) " +
+                                "VALUES ('" + students[i] + "', " +
+                                       "'" + course_id + "', " +
+                                       "'" + hw_id + "', " +
+                                       "'미제출');";
+                cmd.ExecuteNonQuery();
+            }
+
+            con.Close();
+
+            Response.Redirect("hw_list.aspx?current_id=" + ID);
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            //리스트 페이지로 이동 
+            Response.Redirect("hw_list.aspx?current_id=" + ID);
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            subject = DropDownList1.SelectedItem.ToString();
+        }
+
+        /* ------------날짜 dropdownlist------------- */
+        private void DisplayDay(int maxDay)
+        {
+            this.start_day.Items.Clear();
+            this.finish_day.Items.Clear();
+            for (int i = 1; i <= maxDay; i++)
+            {
+                this.start_day.Items.Add(new ListItem(i.ToString()));
+                this.finish_day.Items.Add(new ListItem(i.ToString()));
+            }
+        }
+
+        private void DisplayMonth()
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                this.start_month.Items.Add(new ListItem(i.ToString(), i.ToString()));
+                this.finish_month.Items.Add(new ListItem(i.ToString(), i.ToString()));
+            }
+        }
+
+        private void DisplayYear()
+        {
+            for (int i = DateTime.Now.Year + 1; i > DateTime.Now.Year - 1; i--)
+            {
+                this.start_year.Items.Add(new ListItem(i.ToString()));
+                this.finish_year.Items.Add(new ListItem(i.ToString()));
+            }
+        }
+
+        protected void lstYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetDaysInMonth();
+        }
+
+        private void GetDaysInMonth()
+        {
+            int start_day = DateTime.DaysInMonth(Convert.ToInt32(start_year.SelectedValue), Convert.ToInt32(start_month.SelectedValue));
+            DisplayDay(start_day);
+            int finish_day = DateTime.DaysInMonth(Convert.ToInt32(finish_year.SelectedValue), Convert.ToInt32(finish_month.SelectedValue));
+            DisplayDay(finish_day);
+        }
+
+        protected void lstMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetDaysInMonth();
+        }
+    }
+}
